58bbf4d6dd3467b6ecda65a680e74294746c7c4c
friendsai76 <friendsai76>
2021-07-03 19:03:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.getElementById("demo").innerHTML = "Player 1 is Winner";
+} else if (randomNumber1 < randomNumber2) {
+  document.getElementById("demo").innerHTML = "Player 2 is Winner";
+} else {
+  document.getElementById("demo").innerHTML = "It is a TIE";
+}
